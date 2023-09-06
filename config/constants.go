c840765f5d95f87726d93f940c7f20e1ec13d778
tfkhdyt <me@tfkhdyt.my.id>
2023-09-06 18:35:44 +0700

    fix(config): change configFile to auto-epp-go.conf

--- config/constants.go
+++ config/constants.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	configFile    = "/etc/auto-epp.conf"
+	configFile    = "/etc/auto-epp-go.conf"
 	defaultConfig = `# see available epp state by running: cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences
 [Settings]
 epp_state_for_AC=performance
