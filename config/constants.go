7c9eea9e0452b6652ba2c4f3cacf13e68b6373ae
tfkhdyt <me@tfkhdyt.my.id>
2023-09-28 17:30:40 +0700

    fix(config): change epp_state_for_AC to performance

--- config/constants.go
+++ config/constants.go
@@ -4,7 +4,7 @@ const (
 	configFile    = "/etc/auto-epp-go.conf"
 	defaultConfig = `# see available epp state by running: cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences
 [Settings]
-epp_state_for_AC=balance_performance
+epp_state_for_AC=performance
 epp_state_for_BAT=power
 `
 )
